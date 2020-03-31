4d5b1b58149c0c615560d021c9f704b1d9776090
Artur <aepremyan3993@gmail.com>
2020-03-31 15:55:26 +0300

    commit

--- zakhart/catalog/models.py
+++ zakhart/catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from urllib.request import urlopen
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+
+class Product(models.Model):
+    """Model to present product of person in main page (just now in main)"""
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=200)
+    genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
+    
