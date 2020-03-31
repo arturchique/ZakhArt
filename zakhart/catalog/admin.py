ec041997a96c551fa80ff7954d49e672107b4b10
Artur <aepremyan3993@gmail.com>
2020-03-31 16:28:57 +0300

    Админку сделал

--- zakhart/catalog/admin.py
+++ zakhart/catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Product, Genre
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    fields = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre')
+    fields = ['title', 'author', 'genre', 'summary', ]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
--- zakhart/catalog/models.py
+++ zakhart/catalog/models.py
@@ -7,7 +7,16 @@ import uuid
 class Product(models.Model):
     """Model to present product of person in main page (just now in main)"""
     title = models.CharField(max_length=50)
-    author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=200)
     genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
-    
+
+
+class Genre(models.Model):
+    """Model to present genre"""
+    name = models.TextField
+
+
+class Author(models.Model):
+    """Model to present Author"""
+    name = models.CharField(max_length=50)
