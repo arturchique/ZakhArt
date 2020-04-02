618f2f40c2471e0a8e58ff3f96fafe043a997857
Zakhar <maxpanov32@gmail.com>
2020-04-02 19:38:44 +0300

    adding main_js.js

---
+++ zakhart/catalog/static/js/main_js.js
@@ -0,0 +1,22 @@
+       let str = document.getElementById('link_list').innerHTML;
+       str = str.slice(2,str.length -2);
+       let arr = str.split("', '");
+       let str2 = document.getElementById('url_list').innerHTML;
+       str2 = str2.slice(1,str2.length -1);
+       let arr2 = str2.split(", ");
+       let k =0;
+       let img = document.getElementById("image");
+       img.setAttribute("src", arr[k]);
+       let a = document.getElementById("link_image");
+       a.setAttribute("href", "products/" + arr2[k]);
+       k++;
+       setTimeout(function l(){
+            img.setAttribute("src", arr[k]);
+            a.setAttribute("href","products/" + arr2[k]);
+            k++;
+            if(k==arr.length){
+                k = 0;
+            }
+            setTimeout(l,4000);
+
+        },5000);
