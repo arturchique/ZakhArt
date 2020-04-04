71553be86162747b7bd1eb82196795010e470190
Zakhar <maxpanov32@gmail.com>
2020-04-04 19:53:36 +0300

    added animation

--- zakhart/catalog/static/js/main_js.js
+++ zakhart/catalog/static/js/main_js.js
@@ -22,4 +22,48 @@
         },5000);
 
 //анимация кнопки
-setTimeout(()=>document.getElementsByClassName('circle').style.height = '300px', 2000);
+let element = document.getElementById('circle');
+let my_time = 30;
+let img2 = document.getElementById('image');
+let flag = false;
+function animationRound(){
+    my_time+=1;
+    element.style.transform = "scale(" + 1.05 + "  ," + 1.05 +")";
+    if(my_time > 80){
+        requestAnimationFrame(animationRound2);
+    }
+    else if(flag){
+        element.style.transform = "scale(" + 1 + " ," + 1 +")";
+     }
+    else{
+        requestAnimationFrame(animationRound);
+    }
+}
+function animationRound2(){
+    my_time-=1;
+    element.style.transform = "scale(" + 0.95 + " ," + 0.95 +")";
+    if(my_time < 30){
+        requestAnimationFrame(animationRound);
+       }
+     else if(flag){
+        element.style.transform = "scale(" + 1 + " ," + 1 +")";
+     }
+    else{
+        requestAnimationFrame(animationRound2);
+     }
+}
+let Idreq;
+Idreq = requestAnimationFrame(animationRound);
+
+//анимация раскрытия
+element.onclick = function al(){
+        element.style.borderRadius = "0%";
+        flag = true;
+        element.style.transform = "scale(" + 1 + " ," + 1 +")";
+        element.style.height = "100vh";
+        element.style.width = "70vw";
+        element.style.left = "15vw";
+        element.style.top = "0vh";
+        img2.style.display = "block";
+
+}
